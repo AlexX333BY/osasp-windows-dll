ac24cfe35fe6be4ec2ac3c248c873ba94462769d
Aliaksei <alexx333by@gmail.com>
2018-11-18 16:59:43 +0300

    Added decorated name generator

--- DynamicLoadLibraries/DynamicLoadLibraries.DynamicImportExample/DynamicImportExample.cpp
+++ DynamicLoadLibraries/DynamicLoadLibraries.DynamicImportExample/DynamicImportExample.cpp
@@ -7,6 +7,28 @@ typedef INT64(__stdcall Max_ptr)(INT64 x, INT64 y);
 
 typedef LPSTR(__stdcall GetCurrentProcessName_ptr)();
 
+LPCSTR GetFullFunctionName(LPCSTR lpsFunctionName, size_t iArgumentsSize)
+{
+	UINT uiNameLength = lstrlen(lpsFunctionName) + 2;
+	size_t sSizeCounter = iArgumentsSize;
+	do
+	{
+		sSizeCounter /= 10;
+		++uiNameLength;
+	} while (sSizeCounter > 0);
+
+	LPSTR result = new CHAR[uiNameLength + 1];
+	if (sprintf_s(result, uiNameLength + 1, "_%s@%d", lpsFunctionName, iArgumentsSize) == -1)
+	{
+		delete[] result;
+		return NULL;
+	}
+	else
+	{
+		return result;
+	}
+}
+
 int main()
 {
 	LPCTSTR lpsDllPath = "DynamicLoadLibraries.DllExample.dll";
@@ -18,9 +40,17 @@ int main()
 	}
 	else
 	{
-		Min_ptr *lpMin = (Min_ptr *)GetProcAddress(hLoadedDll, "Min");
-		Max_ptr *lpMax = (Max_ptr *)GetProcAddress(hLoadedDll, "Max");
-		GetCurrentProcessName_ptr *lpGetCurrentProcessName = (GetCurrentProcessName_ptr *)GetProcAddress(hLoadedDll, "GetCurrentProcessName");
+		LPCSTR lpsMinDecorated = GetFullFunctionName("Min", 2 * sizeof(INT64));
+		LPCSTR lpsMaxDecorated = GetFullFunctionName("Min", 2 * sizeof(INT64));
+		LPCSTR lpsGetCurrentProcessNameDecorated = GetFullFunctionName("GetCurrentProcessName", 0);
+
+		Min_ptr *lpMin = (Min_ptr *)GetProcAddress(hLoadedDll, lpsMinDecorated);
+		Max_ptr *lpMax = (Max_ptr *)GetProcAddress(hLoadedDll, lpsMaxDecorated);
+		GetCurrentProcessName_ptr *lpGetCurrentProcessName = (GetCurrentProcessName_ptr *)GetProcAddress(hLoadedDll, lpsGetCurrentProcessNameDecorated);
+
+		delete[] lpsMinDecorated;
+		delete[] lpsMaxDecorated;
+		delete[] lpsGetCurrentProcessNameDecorated;
 
 		if ((lpMin == NULL) || (lpMax == NULL))
 		{
